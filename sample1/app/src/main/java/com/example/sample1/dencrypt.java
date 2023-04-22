9d4a27b93e2a0dac0deb82af027a44582de5bf12
keshav yadav <keshavyadav1324@gmail.com>
2023-04-22 19:32:32 +0530

    App completed

--- sample1/app/src/main/java/com/example/sample1/dencrypt.java
+++ sample1/app/src/main/java/com/example/sample1/dencrypt.java
@@ -1,52 +1,109 @@
-package com.example.sample1;
+    package com.example.sample1;
 
-import androidx.appcompat.app.AppCompatActivity;
+    import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.EditText;
+    import android.widget.ImageView;
+    import android.widget.TextView;
+    import android.widget.Toast;
 
-import java.util.ArrayList;
+    import com.google.firebase.database.DatabaseReference;
+    import com.google.firebase.database.FirebaseDatabase;
 
-public class dencrypt extends AppCompatActivity {
-    private String text;
-    private TextView print;
-    EditText editText;
+    import java.io.File;
+    import java.io.FileOutputStream;
+    import java.io.IOException;
+    import java.util.ArrayList;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dencrypt);
-        Intent intent = getIntent();
-        text = intent.getStringExtra("text");
+    public class dencrypt extends AppCompatActivity {
+        private String text;
+        private String Text;
+        private TextView print;
+        EditText editText;
+        DatabaseReference dbtref;
 
-        print = findViewById(R.id.dn);
-        editText=findViewById(R.id.enterkey);
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_dencrypt);
+            Intent intent = getIntent();
+            text = intent.getStringExtra("text");
 
+            print = findViewById(R.id.dn);
+            editText=findViewById(R.id.enterkey);
+            dbtref= FirebaseDatabase.getInstance().getReference().child("dencrypted data");
 
-    public void startdn(View view){
-        String key= editText.getText().toString();
-        char[] shuffledList = key.toCharArray();
-        ArrayList<Character> list = new ArrayList<>();
-        for(int i = 32; i < 127; i++){
-            list.add((char)i);
+            ImageView exportButton = findViewById(R.id.dde);
+            exportButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Get the encrypted text from your app's data source
+                    String encryptedText = Text;
+
+                    // Call the exportEncryptedText() method to save the encrypted text to a file
+                    exportDencryptedText(encryptedText);
+                }
+            });
         }
-        char[] letter = text.toCharArray();
-        for(int i=0;i<letter.length;i++)
-        {
-            for(int j=0;j<shuffledList.length;j++)
+
+
+        public void startdn(View view){
+            String key= editText.getText().toString();
+
+            char[] shuffledList = key.toCharArray();
+            ArrayList<Character> list = new ArrayList<>();
+            for(int i = 32; i < 127; i++){
+                list.add((char)i);
+            }
+            char[] letter = text.toCharArray();
+            for(int i=0;i<letter.length;i++)
             {
-                if(letter[i]==shuffledList[j])
+                for(int j=0;j<shuffledList.length;j++)
                 {
-                    letter[i]=list.get(j);
-                    break;
+                    if(letter[i]==shuffledList[j])
+                    {
+                        letter[i]=list.get(j);
+                        break;
+                    }
                 }
             }
+            Text = String.valueOf(letter);
+
+            insertdencryptdata();
         }
-        text = String.valueOf(letter);
-        print.setText(text);
-    }
-}
+        private void insertdencryptdata(){
+            String dkey=editText.getText().toString();
+            String value=text;
+            dencryptdata Dencryptdata=new dencryptdata(dkey,value);
+            dbtref.push().setValue(Dencryptdata);
+            Toast.makeText(getApplicationContext(),"data stored",Toast.LENGTH_SHORT).show();
+        }
+
+        public void exportDencryptedText(String encryptedText) {
+            // Create a file object
+            File file = new File(getExternalFilesDir(null), "dencrypted.txt");
+            FileOutputStream fos = null;
+
+            try {
+                // Open a file output stream
+                fos = new FileOutputStream(file);
+
+                // Write the encrypted text to the file
+                fos.write(encryptedText.getBytes());
+
+                // Close the file output stream
+                fos.close();
+
+                // Display a message to the user indicating that the file was saved successfully
+                Toast.makeText(this, " " + file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                // Display an error message to the user
+                Toast.makeText(this, "Error saving encrypted text", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+
+
+    }}
--- sample1/app/src/main/java/com/example/sample1/encrypt.java
+++ sample1/app/src/main/java/com/example/sample1/encrypt.java
@@ -7,19 +7,32 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.FileObserver;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class encrypt extends AppCompatActivity {
     TextView Key;
+    private static final String  filename ="Encrypt.txt";
     ImageView copykey;
     private String text;
+    private String Text;
     private TextView print;
+
+    DatabaseReference dbref;
 
 
     @Override
@@ -30,7 +43,10 @@ public class encrypt extends AppCompatActivity {
         text = intent.getStringExtra("text");
         print = findViewById(R.id.en);
         Key=findViewById(R.id.gkey);
+
+        dbref= FirebaseDatabase.getInstance().getReference().child("encrypted data");
         copykey=findViewById(R.id.copy_key);
+
         copykey.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,16 +57,33 @@ public class encrypt extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"Text copied to cliboard",Toast.LENGTH_SHORT).show();
             }
         });
+
+        ImageView exportButton = findViewById(R.id.ed);
+        exportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the encrypted text from your app's data source
+                String encryptedText = Text;
+
+                // Call the exportEncryptedText() method to save the encrypted text to a file
+                exportEncryptedText(encryptedText);
+            }
+        });
+
+
+
+
     }
 
     ArrayList<Character> shuffledList;
     ArrayList<Character> list;
+    String skey = "";
     private void shuffleList() {
         list = new ArrayList<>();
         for(int i = 32; i < 127; i++){
             list.add((char)i);
         }
-       shuffledList = new ArrayList<>(list);
+        shuffledList = new ArrayList<>(list);
         Collections.shuffle(shuffledList);
     }
 
@@ -65,16 +98,54 @@ public class encrypt extends AppCompatActivity {
                 }
             }
         }
-        text = String.valueOf(letter);
-        print.setText(text);
-    }
+        Text = String.valueOf(letter);
 
+
+    }
     public void getkey(View view) {
         TextView out = findViewById(R.id.gkey);
-        String skey = "";
+
         for(Object x: shuffledList){
             skey += x;
         }
         out.setText(skey);
+        insertencryptdata();
     }
+    private void insertencryptdata(){
+        String key=skey;
+        String value=text;
+        encryptdata Encryptdata=new encryptdata(key,value);
+        dbref.push().setValue(Encryptdata);
+        Toast.makeText(getApplicationContext(),"data stored",Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    public void exportEncryptedText(String encryptedText) {
+        // Create a file object
+        File file = new File(getExternalFilesDir(null), "encrypted.txt");
+        FileOutputStream fos = null;
+
+        try {
+            // Open a file output stream
+            fos = new FileOutputStream(file);
+
+            // Write the encrypted text to the file
+            fos.write(encryptedText.getBytes());
+
+            // Close the file output stream
+            fos.close();
+
+            // Display a message to the user indicating that the file was saved successfully
+            Toast.makeText(this, " " + file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            // Display an error message to the user
+            Toast.makeText(this, "Error saving encrypted text", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
